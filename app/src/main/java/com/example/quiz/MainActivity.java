954f30059f65312cee50b4064c989011773c4bf7
ricardoferreira68 <jrsf68@gmail.com>
2021-11-02 18:15:03 -0300

    Definição do layout

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private final String txtDisciplina = "LINGUAGEM DE MONTAGEM";
+    private final String[] alterantivas = new String[] {
+            "Saída 0, porque o circuito demonstra a falta de fluxo de energia para a saída C.",
+            "A saída C será energizada (saída 1).",
+            "Saída 1, porque a saída C só será zero quando a entrada A for 0 e a entrada B for 0.",
+            "A saída C ficará sem energia (saída 0)."};
+    ListView lvAlternativas = (ListView) findViewById(R.id.idListaAlternativas);
+    ArrayAdapter<String> adpListaAnternativas = new ArrayAdapter<>(this, R.id.idListaAlternativas, alterantivas);
     // private final EditText txtTextoBase = (EditText) findViewById(R.id.idTextoBase);
 
     @Override
