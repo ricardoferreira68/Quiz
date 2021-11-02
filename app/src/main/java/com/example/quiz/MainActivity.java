2da9d9adefd2b36a9ec201ad261b860fc85509ed
ricardoferreira68 <jrsf68@gmail.com>
2021-11-02 19:11:28 -0300

    Definição do layout

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -9,13 +9,11 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private final String txtDisciplina = "LINGUAGEM DE MONTAGEM";
-    private final String[] alterantivas = new String[] {
+    String[] alterantivas = new String[] {
             "Saída 0, porque o circuito demonstra a falta de fluxo de energia para a saída C.",
             "A saída C será energizada (saída 1).",
             "Saída 1, porque a saída C só será zero quando a entrada A for 0 e a entrada B for 0.",
             "A saída C ficará sem energia (saída 0)."};
-    ListView lvAlternativas = (ListView) findViewById(R.id.idListaAlternativas);
-    ArrayAdapter<String> adpListaAnternativas = new ArrayAdapter<>(this, R.layout.lista_anternativas, alterantivas);
     // private final EditText txtTextoBase = (EditText) findViewById(R.id.idTextoBase);
 
     @Override
@@ -23,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle(txtDisciplina);
+        ListView lvAlternativas = (ListView) findViewById(R.id.idListaAlternativas);
+        ArrayAdapter<String> adpListaAnternativas = new ArrayAdapter<String>(this, R.layout.lista_anternativas, alterantivas);
 
         lvAlternativas.setAdapter(adpListaAnternativas);
     }
