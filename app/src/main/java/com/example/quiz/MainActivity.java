3d10a6f40c8f698f567c18ca41da62197c80de72
ricardoferreira68 <jrsf68@gmail.com>
2021-11-02 15:50:38 -0300

    Estrutura do Layout

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private final String txtDisciplina = "LINGUAGEM DE MONTAGEM";
+    // private final EditText txtTextoBase = (EditText) findViewById(R.id.idTextoBase);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(txtDisciplina);
     }
 }
