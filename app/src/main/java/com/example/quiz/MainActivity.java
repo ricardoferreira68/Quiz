4bb32361a7902e862e573b593892eb0fdce02ad1
Ricardo Ferreira <jrsf68@gmail.com>
2021-11-06 19:59:34 -0300

    Finalizada a apresentação da listView

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private View lastSelectedItem;
+    private int respostaCorreta = 1;
+    private int alternativaEscolhida = 2;
     private final String txtDisciplina = "LINGUAGEM DE MONTAGEM";
-    String[] alterantivas = new String[] {
+    String[] alterantivas = new String[]{
             "A) Saída 0, porque o circuito demonstra a falta de fluxo de energia para a saída C.",
             "B) A saída C será energizada (saída 1).",
             "C) Saída 1, porque a saída C só será zero quando a entrada A for 0 e a entrada B for 0.",
@@ -25,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle(txtDisciplina);
+        Button btnResponder = (Button) findViewById(R.id.idBtnResponder);
+        btnResponder.setEnabled(false);
+        Button btnProxima = (Button) findViewById(R.id.idBtnSeguir);
+        btnProxima.setEnabled(false);
         ListView lvAlternativas = (ListView) findViewById(R.id.idListaAlternativas);
         ArrayAdapter<String> adpListaAnternativas = new ArrayAdapter<String>(this, R.layout.lista_anternativas, alterantivas);
 
@@ -38,6 +46,23 @@ public class MainActivity extends AppCompatActivity {
                 }
                 view.setBackgroundColor(getResources().getColor(R.color.black));
                 lastSelectedItem = view;
+                btnResponder.setEnabled(true);
+                alternativaEscolhida = i;
+
+            }
+        });
+
+        btnResponder.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+                btnResponder.setEnabled(false);
+                btnProxima.setEnabled(true);
+                if (alternativaEscolhida == respostaCorreta){
+                    lastSelectedItem.setBackgroundColor(getResources().getColor(R.color.verde_opaco));
+                } else {
+                    lastSelectedItem.setBackgroundColor(getResources().getColor(R.color.vermelho_coral));
+                }
             }
         });
     }
