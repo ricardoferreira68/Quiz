ef82549d41693d9e0bc7f5094e41ffb92995aeb7
Ricardo Ferreira <jrsf68@gmail.com>
2021-11-07 00:56:11 -0300

    Validação de login e senha

--- app/src/main/java/com/example/quiz/Login.java
+++ app/src/main/java/com/example/quiz/Login.java
@@ -1,11 +1,13 @@
 package com.example.quiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
@@ -14,13 +16,26 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btnEntrar = (Button) findViewById(R.id.idBtnEntrar);
+        TextView tvEmail = findViewById(R.id.idTxtLoginEmail);
+        TextView tvSenha = findViewById(R.id.idSenha);
+        Button btnEntrar = findViewById(R.id.idBtnEntrar);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
+                String email = tvEmail.getText().toString();
+                String senha = tvSenha.getText().toString();
+                if (email.equals("")){
+                    Toast.makeText(getApplicationContext(), "Digite o e-mail", Toast.LENGTH_SHORT).show();
+                }else {
+                    if (senha.equals("")) {
+                        Toast.makeText(getApplicationContext(), "Digite a senha.", Toast.LENGTH_SHORT).show();
+                    } else {
+
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
             }
         });
     }
